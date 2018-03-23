400644e9571c3b346db0df4e1287b559dbe32261
Will Scott <wscott@groupon.com>
2018-03-23 10:07:40 +0000

    Reset App for iteration 2

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div className='app'>
+        <header className='app-header'>
+        </header>
+        <main>
+        </main>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker'
 import store from './store'
-import App from './components/app/App'
+import App from './components/App'
 import './index.css'
 
 ReactDOM.render(
