83d4aebfc57c085b879faa3aaad46366692a1670
Will Scott <wscott@groupon.com>
2018-04-04 09:55:43 +0100

    Added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Router, Route } from 'react-router'
+import About from './about/About'
+import Contact from './contact/Contact'
 import './App.css'
 
 export default class App extends Component {
@@ -6,9 +9,17 @@ export default class App extends Component {
     return (
       <div className='app'>
         <header className='app-header'>
+          <p>This is the header</p>
         </header>
-        <main>
+        <main className='app-main'>
+          <Router>
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+          </Router>
         </main>
+        <footer className='app-footer'>
+          <p>This is the footer</p>
+        </footer>
       </div>
     )
   }
